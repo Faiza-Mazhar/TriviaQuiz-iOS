c8b798c56428d6ccb7b86582f081b44e768e5ab4
Faiza Mazhar <faiza.mazhar@bbc.co.uk>
2021-04-22 09:37:09 +0100

    Initial Commit

---
+++ TriviaQuiz/TriviaQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaQuizApp.swift
+//  TriviaQuiz
+//
+//  Created by Faiza Mazhar on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TriviaQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
